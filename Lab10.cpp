5dc2873239c2a09722437d5ca021c8e128b7e6ef
Yaskovich Dmitry <kadavr95@outlook.com>
2016-03-14 21:12:00 +0300

    added comments

--- Lab10.cpp
+++ Lab10.cpp
@@ -1,7 +1,7 @@
-#include <conio.h>
+#include <conio.h>//including input/output libraries
 #include <iostream.h>
 
-void OutputMenu();
+void OutputMenu();//functions prototypes
 int InputArray(int *ArraySize, int **Array);
 int OutputArray(int ArraySize, int *Array);
 int ArithmeticMeanCalculation(int ArraySize, int *Array, float *ArithmeticMean);
@@ -10,46 +10,46 @@ void AboutProgramme();
 void ErrorHandler(int ErrorCode);
 int InputNumber(int *EnteredNumber);
 
-void main()
+void main()//main function
 {
-	short OperationCode;
+	short OperationCode;//defining variables
 	int *Array;
 	int ArraySize=0, ErrorCode;
 	float ArithmeticMean;
-	OutputMenu();
-	while(1)
+	OutputMenu();//show menu of available commands
+	while(1)//endless repeat
 	{
-		OperationCode=getch();
-		system("cls");
-		ErrorCode=0;
-		switch (OperationCode)
+		OperationCode=getch();//get command
+		system("cls");//clear console screen
+		ErrorCode=0;//reset error code
+		switch (OperationCode)//command choice
 		{
 			case 49:
-				ErrorCode=InputArray(&ArraySize, &Array);
+				ErrorCode=InputArray(&ArraySize, &Array);//array input
 				break;
 			case 50:
-				ErrorCode=ArithmeticMeanCalculation(ArraySize, Array, &ArithmeticMean);
+				ErrorCode=ArithmeticMeanCalculation(ArraySize, Array, &ArithmeticMean);//calculation of arithmetic mean
 				break;
 			case 51:
-				ErrorCode=ElementDeletion(&ArraySize, &Array);
+				ErrorCode=ElementDeletion(&ArraySize, &Array);//deletion of element preceding the last maximum element
 				break;
 			case 52:
-				ErrorCode=OutputArray(ArraySize, Array);
+				ErrorCode=OutputArray(ArraySize, Array);//array output
 				break;
 			case 53:
-				AboutProgramme();
+				AboutProgramme();//information about the programme
 				break;
-			default:
-				OutputMenu();
+			default://case of pressing button not assigned to commands
+				OutputMenu();//show menu of available commands
 		}
-		if (ErrorCode!=0)
+		if (ErrorCode!=0)//checking correctness of performed operations
 		{
-			ErrorHandler(ErrorCode);
+			ErrorHandler(ErrorCode);//show error message
 		}
 	}
 }
 
-void OutputMenu()
+void OutputMenu()//show menu of available commands
 {
 	cout<<"MAIN MENU\n";
 	cout<<"Choose operation:\n";
@@ -60,29 +60,29 @@ void OutputMenu()
 	cout<<"Press 5 to get info about the program\n\n";
 }
 
-int InputArray(int *ArraySize, int **Array)
+int InputArray(int *ArraySize, int **Array)//array input
 {
 	int Counter, EnteredNumber, ErrorCode=0;
 	cout<<"ARRAY INPUT\n";
 	cout<<"Enter quantity of elements in array\n";
-	ErrorCode=InputNumber(&EnteredNumber);
+	ErrorCode=InputNumber(&EnteredNumber);//input quantity of elements in array
 	*ArraySize=EnteredNumber;
-	if (*ArraySize<=0)
+	if (*ArraySize<=0)//quantity is zero or below
 	{
 		return 3;
 	}
-	if (ErrorCode!=0)
+	if (ErrorCode!=0)//checking correctness of performed operation
 	{
 		return ErrorCode;
 	}
 	//delete [] *Array;
-	*Array=new int[*ArraySize];
+	*Array=new int[*ArraySize];//create array
 	cout<<"\nEnter array elements\n";
-	for (Counter = 0; Counter < *ArraySize; Counter++)
+	for (Counter = 0; Counter < *ArraySize; Counter++)//input of array elements
 	{
 		 ErrorCode=InputNumber(&EnteredNumber);
 		(*Array)[Counter]=EnteredNumber;
-		if (ErrorCode!=0)
+		if (ErrorCode!=0)//checking correctness of performed operations
 		{
 			return ErrorCode;
 		}
@@ -93,45 +93,45 @@ int InputArray(int *ArraySize, int **Array)
 
 }
 
-int InputNumber(int *EnteredNumber)
+int InputNumber(int *EnteredNumber) //number input
 {
 	int EnteredSymbol, SymbolsQuantity=0;
-	bool ExitCondition=false, NegativeValue=false;
-	*EnteredNumber=0;
-	while (ExitCondition==false)
+	bool NegativeValue=false;
+	*EnteredNumber=0;//reset number
+	while (1)//endless repeat
 	{
-
-		EnteredSymbol=getch()-48;
-		if ((EnteredSymbol>=0)&&(EnteredSymbol<=9))
+		EnteredSymbol=getch()-48;//get code of pressed button and bring it to form of number
+		if ((EnteredSymbol>=0)&&(EnteredSymbol<=9))//if numbers was pressed
 		{
-				*EnteredNumber*=10;
-				*EnteredNumber+=EnteredSymbol;
-				cout<<EnteredSymbol;
-				SymbolsQuantity++;
+				*EnteredNumber*=10;//increase order of magnitude of previous digits
+				*EnteredNumber+=EnteredSymbol;//add new digit
+				cout<<EnteredSymbol;//show new digit
+				SymbolsQuantity++;//increase quantity of symbols
 		}
 		else
 		{
-			if ((EnteredSymbol==-3) && (SymbolsQuantity==0))
+			if ((EnteredSymbol==-3) && (SymbolsQuantity==0))//first symbol is minus sign
 			{
-				cout<<(char)45;
-				NegativeValue=true;
-				SymbolsQuantity++;
+				cout<<(char)45;//show symbol
+				NegativeValue=true;//flag what number is now negative
+				SymbolsQuantity++;//increase quantity of symbols
 			}
-			if ((EnteredSymbol==-40)&&(SymbolsQuantity!=0))			{
-				cout<<(char)8;
+			if ((EnteredSymbol==-40)&&(SymbolsQuantity!=0))//deleting symbol
+			{
+				cout<<(char)8;//delete symbol
 				cout<<" ";
 				cout<<(char)8;
-				*EnteredNumber/=10;
-				if ((NegativeValue==true)&&(SymbolsQuantity==1))
+				*EnteredNumber/=10;//decrease entered number
+				if ((NegativeValue==true)&&(SymbolsQuantity==1))//delete minus sign
 				{
 					NegativeValue=false;
 				}
-				SymbolsQuantity--;
+				SymbolsQuantity--;//decrease quantity of elements
 			}
-			if ((EnteredSymbol==-16) || (EnteredSymbol==-35))
+			if ((EnteredSymbol==-16) || (EnteredSymbol==-35))//end of number input
 			{
-				cout<<(char)32;
-				if (NegativeValue==true)
+				cout<<(char)32;//show space instead of space and enter
+				if (NegativeValue==true)//checking number negativity
 				{
 					*EnteredNumber*=-1;
 				}
@@ -140,86 +140,86 @@ int InputNumber(int *EnteredNumber)
 		}
 	}
 }
-int OutputArray(int ArraySize, int *Array)
+int OutputArray(int ArraySize, int *Array)//array output
 {
 	int Counter;
 	cout<<"ARRAY OUTPUT\n";
-	if (ArraySize>0)
+	if (ArraySize>0)//checking what array has been inputed
 	{
-		cout<<"Array of "<<ArraySize<<" elements\n";
-		for (Counter = 0; Counter < ArraySize; Counter++)
+		cout<<"Array of "<<ArraySize<<" elements\n";//show info about array
+		for (Counter = 0; Counter < ArraySize; Counter++)//output array elements
 		{
 			cout<<Array[Counter]<<" ";
 		}
 		cout<<"\nDone!\n\n";
 	}
 	else
-		return 1;
+		return 1;//array has not been inputed
 	return 0;
 }
 
-int ArithmeticMeanCalculation(int ArraySize, int *Array, float *ArithmeticMean)
+int ArithmeticMeanCalculation(int ArraySize, int *Array, float *ArithmeticMean)//calculating of arithmetic mean
 {
 	int Counter, ElementsSum=0;
 	cout<<"ARITHMETIC MEAN CALCULATION\n";
-	if (ArraySize>0)
+	if (ArraySize>0)//checking what array has been inputed
 	{
-		for (Counter = 0; Counter < ArraySize; Counter++)
+		for (Counter = 0; Counter < ArraySize; Counter++)//sum all elements
 		{
 			ElementsSum+=Array[Counter];
 		}
-		*ArithmeticMean=(float)ElementsSum/ArraySize;
+		*ArithmeticMean=(float)ElementsSum/ArraySize;//calculate average
 		cout<<"Arithmetic mean of array elements is: "<<*ArithmeticMean;
 	}
 	else
-		return 1;
+		return 1;//array has not been inputed
 	return 0;
 }
 
-int ElementDeletion(int *ArraySize, int **Array)
+int ElementDeletion(int *ArraySize, int **Array)//deletion of element preceding the last maximum element
 {
 	int Counter, MaximumElementIndex, MaximumElement;
 	int *ArrayCut;
 	cout<<"ELEMENT DELETION\n";
-	if (*ArraySize>0)
+	if (*ArraySize>0)//checking what array has been inputed
 	{
-		MaximumElementIndex=0;
+		MaximumElementIndex=0;//setting first element as maximum
 		MaximumElement=(*Array)[0];
-		for (Counter = 1; Counter < *ArraySize; Counter++)
+		for (Counter = 1; Counter < *ArraySize; Counter++)//checking all elements
 		{
-			if ((*Array)[Counter]>=MaximumElement)
+			if ((*Array)[Counter]>=MaximumElement)//replacing maximum elemnt
 			{
 				MaximumElement=(*Array)[Counter];
 				MaximumElementIndex=Counter;
 			}
 		}
-		if (MaximumElementIndex>0)
+		if (MaximumElementIndex>0)//checking position of first element
 		{
-			cout<<"Last maximum element is number "<<(MaximumElementIndex+1)<<", with value "<<MaximumElement<<"\n";
+			cout<<"Last maximum element is number "<<(MaximumElementIndex+1)<<", with value "<<MaximumElement<<"\n";//show what will be done
 			cout<<"Preceding element with value "<<(*Array)[MaximumElementIndex-1]<<" has been removed.\n";
-			(*ArraySize)--;
-			MaximumElementIndex--;
-			ArrayCut=new int[*ArraySize];
-			for (Counter = 0; Counter < MaximumElementIndex; Counter++)
+			(*ArraySize)--;//shrink array size
+			MaximumElementIndex--;//get index of element to remove
+			ArrayCut=new int[*ArraySize];//new array of less size
+			for (Counter = 0; Counter < MaximumElementIndex; Counter++)//move elements before removed element to the new array
 			{
 				(ArrayCut)[Counter]=(*Array)[Counter];
 			}
-			for (Counter = MaximumElementIndex; Counter < *ArraySize; Counter++)
+			for (Counter = MaximumElementIndex; Counter < *ArraySize; Counter++)//move elements after removed element to the new array with ыршае
 			{
 			  (ArrayCut)[Counter]=(*Array)[Counter+1];
 			}
-			delete [] *Array;
-			*Array=ArrayCut;
+			delete [] *Array;//delete old array
+			*Array=ArrayCut;//give old array pointer to new array
 		}
 		else
-			return 2;
+			return 2;//first element is a maximum
 	}
 	else
-		return 1;
+		return 1;//array has not been inputed
 	return 0;
 }
 
-void AboutProgramme()
+void AboutProgramme()//show info about the programme
 {
 	cout<<"ABOUT PROGRAMME\n\n";
 	cout<<"Lab 10: Modular programming\n";
@@ -229,10 +229,10 @@ void AboutProgramme()
 	cout<<"Dimini Inc, 2016\n\n";
 }
 
-void ErrorHandler(int ErrorCode)
+void ErrorHandler(int ErrorCode)//show error message
 {
-	cout<<"\nError "<<ErrorCode<<"\n";
-	switch (ErrorCode)
+	cout<<"\nError "<<ErrorCode<<"\n";//show error code
+	switch (ErrorCode)//show error  description
 		{
 			case 1:
 				cout<<"No array found! Enter array first.";
